b68696c7083cc1e3502cd29c0798fe6d4d37044f
dxhunggg <hung071103@gmail.com>
2025-04-18 14:36:38 +0700

    Fix CORS: allow Vercel domain

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://bachhoaxanh-1nwtri2bh-xuan-hung-djinhhs-projects.vercel.app",
+  "https://bachhoaxanh-nhjxtxp2q-xuan-hung-djinhhs-projects.vercel.app",
 ];
 
 app.use(
